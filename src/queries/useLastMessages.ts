9a6291d3d3b67fce70df39bf27e8099d621e81aa
jade-vandal <jade.vandal@doctolib.com>
2023-07-10 14:18:14 +0200

    poll in frontend last message received

---
+++ src/queries/useLastMessages.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+export const useLastMessagesIndex = () =>
+  useQuery(
+    ['last_messages'],
+    async () => {
+      const { data } = await axios.get('/last_messages')
+      return data
+    },
+    { refetchInterval: 5000 }
+  )
--- src/screens/DrawScreen.tsx
+++ src/screens/DrawScreen.tsx
@@ -1,9 +1,11 @@
-import { Button, View } from 'native-base'
+import { Button, Text, View } from 'native-base'
 import DrawCanvas from '../components/DrawCanvas'
 import { useMessageCreate } from '../queries/useMessages'
+import { useLastMessagesIndex } from '../queries/useLastMessages'
 
 const DrawScreen = () => {
   const { mutateAsync: sendMessage } = useMessageCreate()
+  const { data: lastMessageReceived } = useLastMessagesIndex()
   const handleSend = async () => {
     await sendMessage({
       coordinates: [
@@ -20,6 +22,12 @@ const DrawScreen = () => {
     <View flex={1} alignItems="center" justifyContent="center">
       <DrawCanvas />
       <Button onPress={handleSend}>Send</Button>
+      <Text>Last message received</Text>
+      {lastMessageReceived && (
+        <Text>
+          {lastMessageReceived.id} - {lastMessageReceived.content}
+        </Text>
+      )}
     </View>
   )
 }
