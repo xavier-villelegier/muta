8ae9ffbde8492b012355a86f7a83cf073d863b5c
jade-vandal <jade.vandal@doctolib.com>
2023-07-10 12:31:23 +0200

    remove safe params

--- server/app/controllers/messages_controller.rb
+++ server/app/controllers/messages_controller.rb
@@ -2,15 +2,8 @@ class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token # temporary until we implement the login
 
   def create
-    content = JSON.parse(message_params["content"])
-    Message.create!(content: content["coordinates"].to_s)
+    Message.create!(content: params["content"]["coordinates"].to_s)
   rescue JSON::ParserError => e
     render json: { error: "Invalid JSON format" }, status: :unprocessable_entity
-  end
-
-  private
-
-  def message_params
-    params.permit(:content)
   end
 end
