f60335e57564eb5cf8deb7af1bfdc77e3a8dafbf
Xavier Villelégier <xavier.villelegier@doctolib.com>
2023-07-11 12:20:08 +0200

    draw path

--- src/components/ReceivedDrawing.tsx
+++ src/components/ReceivedDrawing.tsx
@@ -1,19 +1,28 @@
-import { Canvas, Path, Skia } from '@shopify/react-native-skia'
+import { Canvas, Path, Skia, runTiming, useValue } from '@shopify/react-native-skia'
 import { Point } from './Draw'
-import { View } from 'native-base'
+import { useEffect } from 'react'
 
 export const ReceivedDrawing = ({ message }: { message: Point[] }) => {
-  if (message === undefined) return null
+  const end = useValue(0)
 
   let path = Skia.Path.Make()
-  path.moveTo(message[0].x, message[0].y)
-  message.map(({ x, y }: Point) => {
-    path.lineTo(x, y)
-  })
+
+  useEffect(() => {
+    if (!message) return
+
+    path.moveTo(message[0].x, message[0].y)
+    message.map(({ x, y }: Point) => {
+      path.lineTo(x, y)
+    })
+
+    runTiming(end, { from: 0, to: 1 }, { duration: 1000 })
+  }, [message])
+
+  if (!message) return null
 
   return (
     <Canvas style={{ flex: 1 }}>
-      <Path path={path} strokeWidth={5} style="stroke" color="#000000" />
+      <Path path={path} strokeWidth={5} style="stroke" color="#000000" start={0} end={end} />
     </Canvas>
   )
 }
